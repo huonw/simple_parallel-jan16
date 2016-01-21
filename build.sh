1d98fa7518fb5985c0e960cd98bc51d6725479f0
Huon Wilson <dbau.pp+github@gmail.com>
2016-01-21 17:44:54 +1100

    Make links the landing page & move slides.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo Running...
-rustdoc index.md -o . --html-in-header=header.inc.html --markdown-no-toc
-rustdoc links.md -o . --markdown-no-toc
+rustdoc slides.md -o . --html-in-header=header.inc.html --markdown-no-toc
+rustdoc index.md -o . --markdown-no-toc
--- links.md
+++
@@ -1,6 +0,0 @@
-% simple_parallel
-
-- [slides](index.html)
-- [talk code](https://github.com/huonw/simple_parallel-jan16)
-- [page on crates.io](https://crates.io/crates/simple_parallel)
-- [code](https://github.com/huonw/simple_parallel)
